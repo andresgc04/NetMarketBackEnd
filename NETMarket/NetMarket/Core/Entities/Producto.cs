5f171cc51a70ea95e2c5b5960e59ab2cadecb379
andresgc04 <andresgc1997@outlook.com>
2021-03-29 18:01:20 -0400

    CreacionDeEntidadesV1

---
+++ NETMarket/NetMarket/Core/Entities/Producto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Producto
+    {
+        public Producto()
+        {
+        }
+
+        [Key]
+        public int ProductoId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Stock { get; set; }
+
+        public int MarcaId { get; set; }
+
+        [Column(TypeName="decimal(18,4)")]
+        public decimal Precio { get; set; }
+
+        public string Imagen { get; set; }
+    }
+}
